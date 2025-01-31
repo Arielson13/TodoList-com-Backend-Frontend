0558fd522c25b173c44aeed59e4185c8bf6709f0
Arielson Sousa Duarte <52675088+Arielson13@users.noreply.github.com>
2025-01-31 00:20:52 -0300

    ultima do dia

--- app/backend/controllers/userController.js
+++ app/backend/controllers/userController.js
@@ -33,7 +33,7 @@ export const login = async (req, res) => {
 
         const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
 
-        res.json({ token })
+        res.json({ status: true, tokenAccess: token })
     } catch (error) {
         res.status(500).json({ message: "Erro no servidor" })
     }
